f93ed3a5f86f89acf995ba5815fe6dad9899f00a
Christian <christianaka65@gmail.com>
2020-10-16 18:58:23 +0000

    connexion

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,6 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private static int identifiant_cand;
+    private static int identifiant_plant;
+    private static String mail;
+    private static String mdp;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -15,6 +19,40 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
+
+    public static String getMdp() {
+        return mdp;
+    }
+
+    public static void setMdp(String mdp) {
+        Main.mdp = mdp;
+    }
+
+    public static String getMail() {
+        return mail;
+    }
+
+    public static void setMail(String mail) {
+        Main.mail = mail;
+    }
+
+    public static int getIdentifiant_cand() {
+        return identifiant_cand;
+    }
+
+    public static int getIdentifiant_plant() {
+        return identifiant_plant;
+    }
+
+    public static void setIdentifiant_cand(int identifiant_cand) {
+        Main.identifiant_cand = identifiant_cand;
+    }
+
+    public static void setIdentifiant_plant(int identifiant_plant) {
+        Main.identifiant_plant = identifiant_plant;
+    }
+
 
 
     public static void main(String[] args) {
--- src/sample/controllers/ConnexionControllers.java
+++ src/sample/controllers/ConnexionControllers.java
@@ -2,22 +2,66 @@ package sample.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import sample.database.AgriConnexion;
 
+import javax.swing.*;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
-public class ConnexionControllers {
+public class ConnexionControllers implements Initializable{
     @FXML
     private TextField identifiant;
-
     @FXML
     private PasswordField mdp;
 
-
+    private String query;
 
     public void connexion(ActionEvent actionEvent) {
+        if(!"".equals(identifiant.getText()) && !"".equals(mdp.getText())) {
+            query = "SELECT * FROM USERS WHERE IDENTIFIANT ='" + identifiant.getText() + "' AND MOT_DE_PASSE='" + mdp.getText() + "'";
+            try {
+                Statement state = AgriConnexion.getInstance().createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet res = state.executeQuery(query);
+                if (res.next()){
+                    ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+                    Parent parent = FXMLLoader.load(getClass().getResource(""));
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(parent);
+                    stage.setScene(scene);
+                    stage.setTitle("MENU");
+                    stage.show();
+                }else {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Information Dialog");
+                    alert.setHeaderText("Identifiant ou Mot de Passe incorrecte");
+                    alert.showAndWait();
+                }
+            } catch (SQLException | IOException sqlException) {
+                JOptionPane.showMessageDialog(null, sqlException.getMessage(), "ERREUR ! ", JOptionPane.ERROR_MESSAGE);
+            }
+        }else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information Dialog");
+            alert.setHeaderText("Veuillez renseignez tous les champs");
+            alert.showAndWait();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
